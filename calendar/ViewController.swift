3e80a3dea1d41081b0137b93b17dd75d918c606a
dididthat <darianikulina02@gmail.com>
2023-04-15 17:57:21 +0300

    начало(кнопка про день)

--- calendar/ViewController.swift
+++ calendar/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelName: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+
     }
 
+    @IBAction func senderButton(_ sender: UIButton) {
+        labelName.backgroundColor = .green
+//        запрос на сайт и получить данные
+        makeRequest(string: "https://isdayoff.ru/20230223")
+    }
+    
+    func makeRequest(string: String) {
+        let request = URLRequest(url: URL(string: string)!)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+        print(String(decoding: data!, as: UTF8.self))
+        print(error)
+        }
+        task.resume()
+    }
 
 }
 
